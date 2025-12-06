cd673805f4935387a458399b4685c38c6426ae13
Christopher Wainwright <231749634+cwainwr3-collab@users.noreply.github.com>
2025-12-06 22:09:50 +0000

    Fixing errors from ruff check

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -94,15 +94,14 @@ class Server:
         @self.app.route("/deanonymizers", methods=["GET"])
         def deanonymizers():
             """Return a list of supported deanonymizers."""
-            return jsonify(self.deanonymize.get_deanonymizers())
-        
+            return jsonify(self.deanonymize.get_deanonymizers())  
         @self.app.route("/genz-preview", methods=["GET"])
-        def genzPreview():
-            """Return example Gen-Z anonymization output"""
+        def genzpreview():
+            """Return example Gen-Z anonymization output."""
             return jsonify({
-                "description": "Example output of the genz anonymizer.",
                 "example": "Call Emily at 577-988-1234",
-                "example output": "Call GOAT at vibe check"
+                "example output": "Call GOAT at vibe check",
+                "description": "Example output of the genz anonymizer."
             })
 
         @self.app.errorhandler(InvalidParamError)
