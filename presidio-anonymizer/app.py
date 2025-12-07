68b9071b5d29ce0f888dad94938b098571b59972
Christopher Wainwright <231749634+cwainwr3-collab@users.noreply.github.com>
2025-12-07 00:12:55 +0000

    Added genz endpoint

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -10,6 +10,8 @@ from presidio_anonymizer import AnonymizerEngine, DeanonymizeEngine
 from presidio_anonymizer.entities import InvalidParamError
 from presidio_anonymizer.services.app_entities_convertor import AppEntitiesConvertor
 from werkzeug.exceptions import BadRequest, HTTPException
+from presidio_anonymizer.operators.genz import GenZ
+from presidio_anonymizer import OperatorConfig
 
 DEFAULT_PORT = "3000"
 
@@ -95,6 +97,7 @@ class Server:
         def deanonymizers():
             """Return a list of supported deanonymizers."""
             return jsonify(self.deanonymize.get_deanonymizers())
+        
         @self.app.route("/genz-preview", methods=["GET"])
         def genzpreview():
             """Return example Gen-Z anonymization output."""
@@ -103,6 +106,30 @@ class Server:
                 "example output": "Call GOAT at vibe check",
                 "description": "Example output of the genz anonymizer."
             })
+        
+        @self.app.route("/genz", methods=["GET"])
+        def genz():
+            content = {
+                "text": "Please contact Emily Carter at 734-555-9284 if you have questions about the workshop registration.",
+                "analyzer_results": [
+                    {"start": 15, "end": 27, "score": 0.3, "entity_type": "PERSON"},
+                    {"start": 31, "end": 43, "score": 0.95, "entity_type": "PHONE_NUMBER"},
+                ]
+            }
+            analyzer_results = AppEntitiesConvertor.analyzer_results_from_json(
+                content["analyzer_results"]
+            )
+            operators = {
+                "PERSON": OperatorConfig("genz"),
+                "PHONE_NUMBER": OperatorConfig("genz"),
+                "DEFAULT": OperatorConfig("genz")
+            }
+            result = self.anonymizer.anonymize(
+                text=content["text"],
+                analyzer_results=analyzer_results,
+                operators=operators,
+            )
+            return result.to_json()
 
         @self.app.errorhandler(InvalidParamError)
         def invalid_param(err):
